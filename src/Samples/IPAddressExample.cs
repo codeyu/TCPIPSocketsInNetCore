a02f98e0464d6f95dcf1ea6450b919309ff7dd58
codeyu <codeyu@gmail.com>
2018-12-28 20:17:15 +0900

    update .net core

--- src/Samples/IPAddressExample.cs
+++ src/Samples/IPAddressExample.cs
@@ -6,43 +6,44 @@ namespace TCPIPSocketsInNetCore
 {
     public class IPAddressExample
     {
-        public static void Run(String host) 
+        public static void Run(string[] args) 
         {
+            var host = Dns.GetHostName();
+            Console.WriteLine($"{host}\n");
             try
             {
-                if(string.IsNullOrEmpty(host))
-                {
-                    host = Dns.GetHostName();
-                }
-                else
-                {
-                    var ipPattern = @"^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$";
-                    IPHostEntry hostInfo;
-                    if(Regex.IsMatch(host,ipPattern))
-                    {
-                        
-                    }
-                    hostInfo = Dns.GetHostEntryAsync(host).Result;
-                    
-                    Console.WriteLine("\tCanonical Name: " + hostInfo.HostName);
-                    Console.Write("\tIP Addresses: ");
-                    foreach (IPAddress ipaddr in hostInfo.AddressList)
-                    {
-                        Console.Write(ipaddr.ToString() + " ");
-                    }
-                    Console.WriteLine();
-                    Console.Write("\tAliases: ");
-                    foreach (String alias in hostInfo.Aliases)
-                    {
-                        Console.Write(alias + " ");
-                    }
-                    Console.WriteLine("\n");
-                }
                 
+                if(args.Length >0  && !string.IsNullOrEmpty(args[0]))
+                {
+                    Console.WriteLine($"{args[0]}\n");
+                    host = args[0];
+                }
+                    
+                //var ipPattern = @"^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$";
+                IPHostEntry hostInfo;
+                
+                hostInfo = Dns.GetHostEntryAsync(host).Result;
+            
+                Console.WriteLine("\tCanonical Name: " + hostInfo.HostName);
+                Console.Write("\tIP Addresses: \n");
+                foreach (IPAddress ipaddr in hostInfo.AddressList)
+                {
+                    Console.Write($"\t{ipaddr.ToString()}\n");
+                }
+                Console.WriteLine();
+                Console.Write("\tAliases: ");
+                foreach (String alias in hostInfo.Aliases)
+                {
+                    Console.Write(alias + " ");
+                }
+                Console.WriteLine("\n");
+                
+
             }
-            catch(Exception)
+            catch(Exception ex)
             {
                 Console.WriteLine("\tUnable to resolve host: " + host + "\n");
+                Console.WriteLine("\tException: " + ex.Message + "\n");
             }
         }
     }
